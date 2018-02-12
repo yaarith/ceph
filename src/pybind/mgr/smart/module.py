fc57a63c9bb46c79060398872553422d992a3b18
Yaarit Hatuka <yaarithatuka@gmail.com>
2018-02-12 08:52:20 -0500

    src/pybind/mgr added scrape commands.

--- src/pybind/mgr/smart/module.py
+++ src/pybind/mgr/smart/module.py
@@ -5,54 +5,125 @@ Pulling SMART data from OSD
 
 import json
 from mgr_module import MgrModule, CommandResult
+import rados
+import time
 
+OBJECT_NAME = "mytest"
+ITER = 2
+pool_name = "smart_data"
 
 class Module(MgrModule):
     COMMANDS = [
         {
+            "cmd": "osd smart scrape "
+                   "name=osd_id,type=CephString,req=true",
+            "desc": "Scraping SMART data of osd<osd_id>",
+            "perm": "r"
+        },
+        {
+            "cmd": "osd smart scrape all",
+            "desc": "Scraping SMART data of all OSDs",
+            "perm": "r"
+        },
+        {
             "cmd": "osd smart get "
                    "name=osd_id,type=CephString,req=true",
-            "desc": "Get smart data for osd_id",
+            "desc": "Get SMART data for osd_id",
             "perm": "r"
         },
         {
             "cmd": "osd smart get all",
-            "desc": "Get smart data for osd.id",
+            "desc": "Get SMART data for all OSDs",
+            "perm": "r"
+        },
+        {
+            "cmd": "pool test",
+            "desc": "Just a pool test",
             "perm": "r"
         },
     ]
 
-    def handle_osd_smart_get(self, osd_id):
+    def open_connection(self):
+        # looking for the right pool
+        # TODO open pool, and if it fails - create and open
+        pools = self.rados.list_pools()
+        is_pool = False
+        for pool in pools:
+            self.log.debug('pool name is %s' % pool)
+            if pool == pool_name:
+                is_pool = True
+                break
+
+        # TODO make pool name configurable
+        if not is_pool:
+            self.rados.create_pool(pool_name)
+            self.log.debug('create %s pool' % pool_name)
+
+        ioctx = self.rados.open_ioctx(pool_name)
+
+        return (ioctx)
+
+    def handle_osd_smart_scrape_all(self):
+        osdmap = self.get("osd_map")
+        assert osdmap is not None
+
+        ioctx = self.open_connection()
+
+        for osd in osdmap['osds']:
+            osd_id = osd['osd']
+            self.do_osd_smart_scrape(str(osd_id), ioctx)
+
+        ioctx.close()
+        return (0, "", "")
+
+    def handle_osd_smart_scrape(self, osd_id):
+        ioctx = self.open_connection()
+        self.do_osd_smart_scrape(osd_id, ioctx);
+        ioctx.close()
+        return (0, "", "")
+
+    def do_osd_smart_scrape(self, osd_id, ioctx):
+        # running 'tell' command on OSD, retrieving smartctl results
         result = CommandResult('')
         self.send_command(result, 'osd', osd_id, json.dumps({
             'prefix': 'smart',
             'format': 'json',
         }), '')
         r, outb, outs = result.wait()
+        smart_data = json.load(bytes(outb))
 
-        return (r, outb, outs)
+        osd_host = self.get_metadata('osd', osd_id)['hostname']
+        self.log.debug('osd host %s' % osd_host)
+
+        for device, device_smart_data in smart_data.items():
+            object_name = osd_host + ":" + device
+            with rados.WriteOpCtx() as op:
+                # TODO should we change second arg to be byte?
+                # TODO what about the flag in ioctx.operate_write_op()?
+                now = str(time.time())
+                ioctx.set_omap(op, now, (str(json.dumps(device_smart_data)),))
+                ioctx.operate_write_op(op, object_name)
+                self.log.debug('writing object %s, key: %s, value: %s' % object_name, now, str(json.dumps(device_smart_data)))
+
+        return (0, "do osd scrape", "test")
 
     def handle_command(self, cmd):
         self.log.error("handle_command")
 
-        if cmd['prefix'] == "osd smart get":
-            r, outb, outs = self.handle_osd_smart_get(cmd['osd_id'])
+        if cmd['prefix'] == "osd smart scrape":
+            return self.handle_osd_smart_scrape(cmd['osd_id'])
 
-            return (r, outb, outs)
+        elif cmd['prefix'] == "osd smart scrape all":
+            return self.handle_osd_smart_scrape_all()
+
+        elif cmd['prefix'] == "osd smart get":
+            return (0, "get", "test")
 
         elif cmd['prefix'] == "osd smart get all":
-            osdmap = self.get("osd_map")
-            assert osdmap is not None
+            return (0, "get all", "test")
 
-            smart_map = {}
-            # TODO what do we do in case of many osds? should we query all osds concurrently?
-            for osd in osdmap['osds']:
-                osd_id = osd['osd']
-                r, outb, outs = self.handle_osd_smart_get(str(osd_id))
-                smart_map[osd_id] = json.loads(outb)
-
-            # TODO do we need to keep the other r and outs values?
-            return (r, json.dumps(smart_map), outs)
+        elif cmd['prefix'] == "pool test":
+            return (0, "pool", "test")
 
         else:
             # mgr should respect our self.COMMANDS and not call us for
